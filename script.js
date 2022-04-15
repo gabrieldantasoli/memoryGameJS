9335eba5c5f41ebade0912dae0bb826eaea1ca55
Gabriel Dantas de Oliveira <gabriel36dantas@gmail.com>
2022-04-14 22:31:28 -0300

    Memory Game

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+var cards = [] ;
+
 var start = document.querySelector('#start') ;
 start.addEventListener('click',checkCanStart) ;
 
@@ -28,6 +30,8 @@ function checkCanStart(){
 } ;
 
 function startGame() {
+    cards.clear ;
+    setCards() ;
     document.querySelector('footer').classList.toggle('active') ;
     document.querySelector('#gameOptions').style.display = 'none' ;
     document.querySelector('#gameContainer').style.display = 'initial' ;
@@ -51,7 +55,36 @@ function startGame() {
         div.style.left = `calc(${((100 / Math.sqrt(gameDificulty)))*index}% + 1px)` ;
         div.style.width = `calc(${100 / Math.sqrt(gameDificulty)}% - 2px)` ;
         div.style.height = `calc(${100 / Math.sqrt(gameDificulty)}% - 2px)` ;
+        let divSimbol = document.createElement('div') ;
+        let divNumber = document.createElement('div') ;
+        divSimbol.classList.add('simbol') ;
+        divNumber.classList.add('num') ;
         document.querySelector('#divGame').append(div) ;
         index += 1 ;
+    } ;
+
+    var divs = document.querySelectorAll('#divGame div')
+    for (let p = 0; p<divs.length;p++) {
+        divs[p].textContent = cards[p]['number']
+    } ;
+} ;
+
+function setCards() {
+    var list = [] ;
+    for (let index = 1; index < (gameDificulty / 2) + 1; index++){
+        list.push(index) ;
+        list.push(index) ;
+    } ;
+    console.log(list)
+    for (let index = 0; index < gameDificulty; index++){
+        cards[index] = {number: null,'status': 1} ;
+        while (true) {
+            let position = parseInt(Math.random() * (gameDificulty + 1)) ;
+            if (list[position] != null){
+                cards[index]['number'] = list[position] ;
+                list[position] = null ;
+                break ;
+            } ;
+        } ;
     } ;
 } ;
